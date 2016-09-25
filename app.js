7a49d4bdb3ca2fc3fee49cb316265c1aa3bf7ed5
wjj <13524220689@189.cn>
2016-09-25 20:06:44 +0800

    good item component finish

---
+++ app.js
@@ -0,0 +1,17 @@
+App({
+  globalData: {
+    hasLogin: false    
+  },
+
+  onLaunch() {
+    console.log('App Init');    
+  },
+
+  onShow() {
+    console.log('App Show');    
+  },
+
+  onHide() {
+    console.log('App Hide');
+  }    
+})
